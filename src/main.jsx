18160107a5cd4abf3a416382df451ce7ca81d18d
Faysal-Sohan <83866915+Faysal-Sohan@users.noreply.github.com>
2023-09-25 22:57:14 +0600

    react router, tailwind, daisiui setup

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Roots from './components/Roots/Roots';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Roots></Roots>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
